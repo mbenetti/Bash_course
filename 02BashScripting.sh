c77c4fce79eac28c3c9e52363ba395b75b79dbf7
mbenetti <mbenetti@hotmail.es>
2021-02-12 15:02:27 +0100

    cimmit

---
+++ 02BashScripting.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/bash
+# ------------------------------------------
+
+# This tutorial will conver the following list:
+
+# 01) Hello Bash Scripting
+# 02) Redirect to file
+# 03) Comments
+# 04) Conditional Statements
+# 05) Loops
+# 06) Script input
+# 07) Script output
+# 08) how to send output from one script to another scrpt
+# 09) String Processing
+# 10) Numbers and Arithmetic
+# 11) Declare Command
+# 12) Arrays
+# 13) Functions
+# 14) Files and Directories
+# 15) Send Email Via Script
+# 16) Curl in Scripts
+# 17) Professional Menus
+# 18) Wait for filesystem events with inotify
+# 19) Introduction to grep
+# 20) Introduction to awk
+# 21) Introduction to sed
+# 22)  Debugging bash scripts
+
+# -----------------------------------------
+
+# 0 - Scrpting options, this shows all posibles shells in out system
+# cat /etc/shells
+# touch myscript.sh
+# 
